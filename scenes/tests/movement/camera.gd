2b603761f5942fce214745b9fda84f006f804fc0
ioxu <ben@ioxu.com>
2023-07-20 15:17:51 +0100

    pretty bad camera position clamping

--- scenes/tests/movement/camera.gd
+++ scenes/tests/movement/camera.gd
@@ -8,14 +8,38 @@ extends Camera2D
 var vel : Vector2
 var displacement := Vector2.ZERO
 
+var margin_vertical := 100
+var margin_horizontal := 450
+
+@onready var wsize = DisplayServer.window_get_size(0)
+@onready var c_limits := {
+	left = 0,
+	top = 0,
+	right = wsize.x,
+	bottom = wsize.y
+}
+
+
 func _ready():
-	displacement = DisplayServer.window_get_size(0)/2.0
+	displacement = wsize/2.0
+	c_limits.left = margin_horizontal
+	c_limits.top = margin_vertical
+	c_limits.right = wsize.x - margin_horizontal
+	c_limits.bottom = wsize.y - margin_vertical
+	print("camera limits: ")
+	print("    %s"%c_limits.left)
+	print("    %s"%c_limits.top)
+	print("    %s"%c_limits.right)
+	print("    %s"%c_limits.bottom)
 
 
 func _process(delta):
 
 	#https://www.youtube.com/watch?v=YBgCUQVDRkw&ab_channel=MrElipteach
-	vel = tracking_object.global_position - self.global_position
+	var track_pos = tracking_object.global_position
+	track_pos.x = clamp( track_pos.x, c_limits.left, c_limits.right )
+	track_pos.y = clamp( track_pos.y, c_limits.top, c_limits.bottom )
+	vel =  track_pos - self.global_position
 	var force : Vector2 = (-spring * displacement) - (damp * vel)
 	vel += force * delta
 	displacement += vel * delta
